bfab0f3a371697b2961d5f7737ec063e439b8cbd
usopp <usopp9@naver.com>
2018-04-17 10:29:39 +0900

    SubActivity 추가 및 MainActivity 수정

--- app/src/main/java/kr/or/dgit/it/helloandroid/MainActivity.java
+++ app/src/main/java/kr/or/dgit/it/helloandroid/MainActivity.java
@@ -1,7 +1,10 @@
 package kr.or.dgit.it.helloandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void nBtnClick(View view) {
+        Toast.makeText(this, "버튼을 눌렀습니다. 하드코딩",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, R.string.btnClickMsg,Toast.LENGTH_LONG).show();
+        startActivity(new Intent(getApplicationContext(), SubActivity.class));
+
     }
 }
